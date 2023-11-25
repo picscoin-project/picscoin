4ead863574d98a41d99efd257af038cfac4c1ffc
picscoin-project <151029182+picscoin-project@users.noreply.github.com>
2023-11-25 06:21:31 +0000

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -236,10 +236,7 @@ public:
 
         vFixedSeeds.clear();
         vSeeds.clear();
-        // nodes with support for servicebits filtering should be at the top
-       // vSeeds.emplace_back("testnet-seed.picscoins.org");
-        //vSeeds.emplace_back("seed-b.picscoin.loshan.co.uk");
-        //vSeeds.emplace_back("dnsseed-testnet.thrasher.io");
+
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,117);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
